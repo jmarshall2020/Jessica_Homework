2340d8359138cd48f882e9f4cdd78d1c9c9d687a
Jessica Marshall <jmarshall2202020@gmail.com>
2019-12-05 20:31:21 -0800

    SQLalchemy

---
+++ SQLalchemy/sqlalchemy-challenge/app.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session=Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Measurement).all()
+    prcp_data = []
+    for precipitation in results:
+        results_dict = {}
+        results_dict["date"] = precipitation.date
+        results_dict["percipitation"] = precipitation.prcp
+        prcp_data.append(results_dict)
+
+    return jsonify(prcp_data)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+   
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Station).all()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_stations = []
+    for station in results:
+        station_dict = {}
+        station_dict["station"] = station.station
+        
+        all_stations.append(station_dict)
+
+    return jsonify(all_stations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
